83f1f9625b88698b4ab337308da6241e82cfb780
atha G <74566464+Pandademic@users.noreply.github.com>
2022-05-17 18:04:30 -0400

    Update atha-ui.el

--- atha-ui.el
+++ atha-ui.el
@@ -18,8 +18,7 @@
                '(right-fringe   . 1)
                '(tool-bar-lines . 0)
                '(menu-bar-lines . 0))))
-(if atha-font-name == nil
-    (ignore)   	
+(if not(atha-font-name) 
   (add-to-list 'default-frame-alist '(font . atha-font-name)
   (set-face-attribute 'default t :font atha-font-name ))
 
