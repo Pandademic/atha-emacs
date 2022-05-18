808c46a3c5f68fe4ca63352ef5a7409103e8baaa
atha G <74566464+Pandademic@users.noreply.github.com>
2022-05-18 16:23:20 -0400

    Update atha-ui.el

--- atha-ui.el
+++ atha-ui.el
@@ -1,6 +1,6 @@
 
 ;; no scroll bar menu bar or tool bar ( If found )
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))\
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))      
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))   
 
