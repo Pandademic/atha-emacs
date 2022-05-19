c73921ec9a47fb6e9a8b8375d94acf322c2386d5
atha G <74566464+Pandademic@users.noreply.github.com>
2022-05-19 05:55:32 -0400

    Update atha-defaults.el

--- atha-defaults.el
+++ atha-defaults.el
@@ -1,12 +1,5 @@
+(setq atha-font-name "")
 
-;; y/n
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; UTF-8 everywhere
-(prefer-coding-system       'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-language-environment   'utf-8)
+(setq atha-font-size 117)
 
 (provide 'atha-defaults)
