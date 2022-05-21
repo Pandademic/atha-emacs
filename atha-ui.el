f8092b1c39c886ade1beb2b72c45e3c729cf2356
atha G <74566464+Pandademic@users.noreply.github.com>
2022-05-21 08:55:21 -0400

    finish making bar's toggleable

--- atha-ui.el
+++ atha-ui.el
@@ -4,9 +4,9 @@
 ;; Licenced under the MIT licence<https://raw.githubusercontent.com/Pandademic/atha-emacs/main/LICENSE>
 
 ;; no scroll bar menu bar or tool bar ( If found )
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))      
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))   
+(if atha-show-scrollbar (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1)))
+(if atha-show-toolbar (if fboundp 'tool-bar-mode) (tool-bar-mode -1)))      
+(if atha-show-menubar (if fboundp 'menu-bar-mode) (menu-bar-mode -1)))  
 
 ;; pad window
 
@@ -24,6 +24,17 @@
                '(menu-bar-lines . 0))))
 ;; user font size
 (set-face-attribute 'default nil :height atha-font-size)
+
+
+(modify-all-frames-parameters
+ '((right-divider-width . 40)
+   (internal-border-width . 40)))
+(dolist (face '(window-divider
+                window-divider-first-pixel
+                window-divider-last-pixel))
+  (face-spec-reset-face face)
+  (set-face-foreground face (face-attribute 'default :background)))
+(set-face-background 'fringe (face-attribute 'default :background))
 
 
 (provide 'atha-ui)
